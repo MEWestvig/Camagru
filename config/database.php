2a15a6f59dd26cebcb3a27c63e4394f25490b34a
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-10-29 10:51:57 +0200

    Pushing to laptop

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 $name = 'camagru';
-$dsn = "mysql:host=localhost:3306;dbname=".$name;
-$user = 'camagru';
-$password = 'password';
+$dsn = "mysql:host=localhost:3306";
+$user = 'root';
+$password = 'Westvig1';
 ?>
--- config/setup.php
+++ config/setup.php
@@ -1,11 +1,15 @@
 <?php
 require_once('database.php');
+
 try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = file_get_contents('data.sql');
-    $qr = $db->exec($sql);
+    $sql = "CREATE DATABASE camagru";
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+$sql = file_get_contents('data.sql');
+$qr = $db->execute($sql);
 ?>
